dd7091ef440dcb36b42f735d4287427e50aa5197
Dylan Charleston <dcharleston@opentable.com>
2021-06-23 16:21:14 -0700

    Validate config before reloading go-carbon

--- manifests/service.pp
+++ manifests/service.pp
@@ -14,6 +14,7 @@ define go_carbon::service(
       File[$go_carbon::whisper_aggregation_file]
     ]
     refreshonly => true,
+    onlyif      => "/usr/bin/go-carbon -config ${go_carbon::config_dir}/${service_name}.conf -check-config=true"
   }
 
   case $::operatingsystemmajrelease {
